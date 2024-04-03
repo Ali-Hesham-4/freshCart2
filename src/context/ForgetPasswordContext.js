75f0e2889c0aeb7922c5f66483c87c0bc18a665d
Ali Hesham <alibasher2013@gmail.com>
2024-04-03 21:15:29 +0200

    upload

---
+++ src/context/ForgetPasswordContext.js
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react"
+
+export const ForgetPasswordContext = createContext()
+
+export default function ForgetPasswordContextProvider( { children } ) {
+
+    const [ email , setEmail] = useState("")
+
+
+return <> 
+<ForgetPasswordContext.Provider value = { {
+    email,
+    setEmail
+} }>
+
+
+{ children } 
+
+
+</ForgetPasswordContext.Provider>
+
+
+
+</>
+
+}
